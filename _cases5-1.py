8e073d20be38a6c7f7aa9466d32f65d1ce07f219
adilmaharlam <adilmaharlam1@gmail.com>
2026-05-11 13:08:17 +0400

    cases 5-2 complete

---
+++ _cases5-1.py
@@ -0,0 +1,7 @@
+car = ['mercedes', 'lambo' , 'bugatti', 'ferrari']
+print("IS_car == 'mercedes'? I predict True.")
+print(car== 'mercedes')
+print()
+print(car== 'tofas')
+print()
+print(car== 'lambo')
---
+++ _cases5-2.py
@@ -0,0 +1,36 @@
+fruit = 'banana'
+print(fruit== 'banana')
+print(fruit=='apple')
+
+print()
+
+name  = 'Adil'
+print(name.lower()== 'adil')
+print(name.lower()== 'Adil')
+
+print()
+
+age = 20
+print(age >= 20)
+print(age < 18)
+
+print()##############################
+
+gpa_1 = 3.5
+gpa_2 = 2.8
+print( gpa_1 >= 3 and gpa_2 >= 3)
+print( gpa_1 >= 3 and gpa_2 <= 3)
+
+print()##############################
+
+techstack = ['python', 'java', 'C++']
+print('kotlin' in techstack)
+print('java' in techstack)
+
+print()##############################
+
+bannedusers = ['burxan','amin','hesen']
+user = 'rafael'
+
+print('rafael' not in bannedusers)
+print('amin' not in bannedusers)
